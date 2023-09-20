c9809b0f47e22212ad5844243a4866b950ff2335
Noodles <yilangcharleshe@gmail.com>
2023-09-19 21:10:06 -0400

    Initial Commit

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOApp.swift
+//  TODO
+//
+//  Created by Charles He on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
